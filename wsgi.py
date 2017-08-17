651ec30055b6a23b2004d555055daf4cd07a48ea
kslappey <kristen.slappey@optum.com>
2017-08-17 08:42:21 -0400

    adding api to hello world config

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,49 @@
-from flask import Flask
-application = Flask(__name__)
+#!flask/bin/python
+from flask import Flask, jsonify
 
-@application.route("/")
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route("/")
 def hello():
     return "Hello World!"
 
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+from flask import abort
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+from flask import make_response
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == "__main__":
-    application.run()
+    app.run()
